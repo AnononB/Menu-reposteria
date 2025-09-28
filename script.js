712f2ed2f2244e91c8dc42ed0ac016436441de6d
Ricardo García <hazelgrmtz8@gmail.com>
2025-09-28 00:49:47 -0600

    Inicio del script con JS. Modificación de algunos colores (css).

---
+++ script.js
@@ -0,0 +1,40 @@
+//Datos del menú
+const menuDatos = {
+    "pays": [
+        {
+            "nombre": "Pay de queso (individual)",
+            "descripción": "Pay de queso, horneado, relleno con queso crema, galleta (corteza) crujiente.",
+            "precio": "$15.00 MX",
+            /*"imagen" : "direccion de la imagen",*/
+            "categoria": "pays"
+        },
+
+        {
+
+            "nombre": "Pay de queso (Familiar)",
+            "descripción": "Pay de queso, horneado, relleno con queso crema, galleta (corteza) crujiente.",
+            "precio": " $250.00 MX",
+            "categoria": "pays"
+        },
+
+        {
+            "nombre": "Pay de nuez (individual)" ,
+            "descripcion": "Pay de queso crema con relleno de nuez, corteza semi crujiente",
+            "precio": "$20.00 MX",
+            //"imagen": ,
+            "categoria": "pays" 
+        },
+
+        {
+
+            "nombre": "Pay de nuez (Familiar)" ,
+            "descripcion": "Pay de queso crema con relleno de nuez, corteza semi crujiente",
+            "precio": "$270.00 MX" ,
+            //"imagen": ,
+            "categoria": "pays" 
+        }
+           
+    ],
+
+    
+}
